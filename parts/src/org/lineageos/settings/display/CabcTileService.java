rockstar5495/android_device_xiaomi_sweet

sweet: parts: Add support for HBM and CABC
 * write preferences and qs tiles
 * enable hbm by default
 * enable cabc by default
 * address sepolicy denials

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

parts/src/org/lineageos/settings/display/CabcTileService.java
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2021 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.display;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import java.util.Arrays;
+
+import org.lineageos.settings.R;
+
+public class CabcTileService extends TileService {
+
+    private Context context;
+    private Tile tile;
+
+    private String[] CabcModes;
+    private String[] CabcValues;
+    private int currentCabcMode;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        CabcModes = context.getResources().getStringArray(R.array.amoled_cabc_modes);
+        CabcValues = context.getResources().getStringArray(R.array.amoled_cabc_values);
+    }
+
+    private void updateCurrentCabcMode() {
+        currentCabcMode = Arrays.asList(CabcValues).indexOf(SystemProperties.get(AmoledFeaturesPreferenceFragment.CABC_PROP, "0"));
+    }
+
+    private void updateCabcTile() {
+        tile.setState(currentCabcMode > 0 ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setContentDescription(CabcModes[currentCabcMode]);
+        tile.setSubtitle(CabcModes[currentCabcMode]);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        tile = getQsTile();
+        updateCurrentCabcMode();
+        updateCabcTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        updateCurrentCabcMode();
+        if (currentCabcMode == CabcModes.length - 1) {
+            currentCabcMode = 0;
+        } else {
+            currentCabcMode++;
+        }
+        SystemProperties.set(AmoledFeaturesPreferenceFragment.CABC_PROP, CabcValues[currentCabcMode]);
+        updateCabcTile();
+    }
+}

parts/src/org/lineageos/settings/display/HbmTileService.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2021 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.display;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import org.lineageos.settings.R;
+
+public class HbmTileService extends TileService {
+
+    private Context context;
+    private Tile tile;
+
+    private String[] HbmModes;
+    private String[] HbmValues;
+    private int currentHbmMode;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        HbmModes = context.getResources().getStringArray(R.array.amoled_hbm_modes);
+        HbmValues = context.getResources().getStringArray(R.array.amoled_hbm_values);
+    }
+
+    private void updateCurrentHbmMode() {
+        currentHbmMode = SystemProperties.getInt(AmoledFeaturesPreferenceFragment.HBM_PROP, 0);
+    }
+
+    private void updateHbmTile() {
+        tile.setState(currentHbmMode > 0 ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setContentDescription(HbmModes[currentHbmMode]);
+        tile.setSubtitle(HbmModes[currentHbmMode]);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        tile = getQsTile();
+        updateCurrentHbmMode();
+        updateHbmTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        updateCurrentHbmMode();
+        if (currentHbmMode == HbmModes.length - 1) {
+            currentHbmMode = 0;
+        } else {
+            currentHbmMode++;
+        }
+        SystemProperties.set(AmoledFeaturesPreferenceFragment.HBM_PROP, Integer.toString(currentHbmMode));
+        updateHbmTile();
+    }
+}

rockstar5495/platform_device_xiaomi_sweet

 * kang icons from realmeparts
Signed-off-by: AtomicXZ <achubadyal4@gmail.com>
Signed-off-by: redcliff-op <devyashjangid@gmail.com>

parts/src/org/lineageos/settings/display/CabcTileService.java
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2021 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.display;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import java.util.Arrays;
+
+import org.lineageos.settings.R;
+
+public class CabcTileService extends TileService {
+
+    private Context context;
+    private Tile tile;
+
+    private String[] CabcModes;
+    private String[] CabcValues;
+    private int currentCabcMode;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        CabcModes = context.getResources().getStringArray(R.array.lcd_cabc_modes);
+        CabcValues = context.getResources().getStringArray(R.array.lcd_cabc_values);
+    }
+
+    private void updateCurrentCabcMode() {
+        currentCabcMode = Arrays.asList(CabcValues).indexOf(SystemProperties.get(LcdFeaturesPreferenceFragment.CABC_PROP, "0"));
+    }
+
+    private void updateCabcTile() {
+        tile.setState(currentCabcMode > 0 ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setContentDescription(CabcModes[currentCabcMode]);
+        tile.setSubtitle(CabcModes[currentCabcMode]);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        tile = getQsTile();
+        updateCurrentCabcMode();
+        updateCabcTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        updateCurrentCabcMode();
+        if (currentCabcMode == CabcModes.length - 1) {
+            currentCabcMode = 0;
+        } else {
+            currentCabcMode++;
+        }
+        SystemProperties.set(LcdFeaturesPreferenceFragment.CABC_PROP, CabcValues[currentCabcMode]);
+        updateCabcTile();
+    }
+}

parts/src/org/lineageos/settings/display/HbmTileService.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2021 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.display;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import org.lineageos.settings.R;
+
+public class HbmTileService extends TileService {
+
+    private Context context;
+    private Tile tile;
+
+    private String[] HbmModes;
+    private String[] HbmValues;
+    private int currentHbmMode;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        HbmModes = context.getResources().getStringArray(R.array.lcd_hbm_modes);
+        HbmValues = context.getResources().getStringArray(R.array.lcd_hbm_values);
+    }
+
+    private void updateCurrentHbmMode() {
+        currentHbmMode = SystemProperties.getInt(LcdFeaturesPreferenceFragment.HBM_PROP, 0);
+    }
+
+    private void updateHbmTile() {
+        tile.setState(currentHbmMode > 0 ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setContentDescription(HbmModes[currentHbmMode]);
+        tile.setSubtitle(HbmModes[currentHbmMode]);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        tile = getQsTile();
+        updateCurrentHbmMode();
+        updateHbmTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        updateCurrentHbmMode();
+        if (currentHbmMode == HbmModes.length - 1) {
+            currentHbmMode = 0;
+        } else {
+            currentHbmMode++;
+        }
+        SystemProperties.set(LcdFeaturesPreferenceFragment.HBM_PROP, Integer.toString(currentHbmMode));
+        updateHbmTile();
+    }
+}