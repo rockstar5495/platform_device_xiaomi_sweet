04e25014a6e850af7470ef2bf0e9230a0d6fddc8
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-05-14 19:00:07 +0000

    sweet: cleanup fingerprint and description * pick from source
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- blaze_sweet.mk
+++ blaze_sweet.mk
@@ -28,8 +28,3 @@ PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
-
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
