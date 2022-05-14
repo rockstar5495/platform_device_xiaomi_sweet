a7aea98f6a6a3733ed3fb9b6cc8061529ab448f2
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-05-14 15:28:08 +0000

    sweet: build vendor.qti.hardware.camera.device@1.0 as vendor * Required for prebuilt camera providers * vndksupport: Could not load /vendor/lib64/hw/android.hardware.camera.provider@2.4-impl.so from default namespace: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found: needed by /vendor/lib64/camera.device@1.0-impl.so in namespace (default).
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -219,7 +219,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    android.hardware.camera.provider@2.4-external
+    android.hardware.camera.provider@2.4-external \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libshim_megvii \
